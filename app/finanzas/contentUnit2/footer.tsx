dfba2afe3995dd3809e1235e8324a605699e9c13
QoriEcu <qoriecu@gmail.com>
2024-06-05 22:07:29 -0500

    buttom back

--- app/finanzas/contentUnit2/footer.tsx
+++ app/finanzas/contentUnit2/footer.tsx
@@ -1,22 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export const FooterContent = () => {
-    return(
-        <footer className="hidden lg:block h-20 w-full border-t-2 borde-slate-200 p-2">
+    return (
+        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
             <div className="max-w-screen-lg mx-auto flex items-center justify-center h-full">
-        
-                <Button
-                    variant="secondary"
-                    size="lg"
-                >
-                    
-                    <Link href="/guia/finanzas">
+                <Link href="/guia/finanzas" passHref>
+                    <Button as="a" variant="secondary" size="lg">
                         Back
-                    </Link>
-                </Button>
-
+                    </Button>
+                </Link>
             </div>
         </footer>
     );
